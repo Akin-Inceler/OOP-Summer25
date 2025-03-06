36d30d530ce5b335dc8e61433295f699182504c9
Akin-Inceler <akin2001inceler@gmail.com>
2025-03-06 12:12:34 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 class Person:
-    def __init__(self, name, age, address):
+    def __init__(self, name, age, index, nationality):
         self.name = name
         self.age = age
-        self.address = address
+        self.index = index
+        self.nationality = nationality
 
     def greet(self):
-        return f"Hello, my name is {self.name} and I am {self.age} years old. My address is {self.address}"
-p1 = Person("Akin Inceler" , 23, "Zaczek Dormitory")
+        return f"Hello, my name is {self.name} and I am {self.age} years old. My index number {self.index} and nationality {self.nationality}"
+p1 = Person("Akin Inceler" , 23, 25525, "Turkey")
+p2= Person("Ahmet Solmaz", 20, 25654, "Turkey")
+p3=Person("John Dom", 21, 25956, "USA")
 print(p1.greet())
+print(p2.greet())
+print(p3.greet())
