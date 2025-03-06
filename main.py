e73a5f3b872533b5d9b17ab60e60812156dd9d21
Akin-Inceler <akin2001inceler@gmail.com>
2025-03-06 11:17:03 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-# change it to print your name
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-print("Hello, I am Piotr Brudny")
+    def greet(self):
+        return f"Hello, my name is {self.name} and I am {self.age} years old."
+p1 = Person("Akin Inceler" , 23)
+print(p1.greet())
