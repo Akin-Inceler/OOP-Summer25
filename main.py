3ba852ffc9290f0b4d0de1be35e70d50d97c73ee
Akin-Inceler <akin2001inceler@gmail.com>
2025-03-11 10:33:28 +0000

    Assign data structures to variables

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-class Person:
-    def __init__(self, name, age, index, nationality):
-        self.name = name
-        self.age = age
-        self.index = index
-        self.nationality = nationality
-
-    def greet(self):
-        return f"Hello, my name is {self.name} and I am {self.age} years old. My index number {self.index} and nationality {self.nationality}"
-p1 = Person("Akin Inceler" , 23, 25525, "Turkey")
-p2= Person("Ahmet Solmaz", 20, 25654, "Turkey")
-p3=Person("John Dom", 21, 25956, "USA")
-print(p1.greet())
-print(p2.greet())
-print(p3.greet())
+my_age = 4  # integer
+pi_value = 3.14  # float
+user_name = "JohnDoe"  # string
+list_95 = [10,5.3,"hi"] #list
+is_active = True  # boolean
+dictionaries = {"key": "fire","oto":"volvo"} # dict
+tupp= (5,52.4,"ja") # tuples
+sets = {"a","b","x"} # set
