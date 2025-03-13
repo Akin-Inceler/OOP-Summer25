18eb6227fd7ff0912f139fe670d28fa31dcab083
Akin-Inceler <akin2001inceler@gmail.com>
2025-03-13 10:56:39 +0000

    Assign data structures to variables

--- basic_data_structures.py
+++ basic_data_structures.py
@@ -1,1 +1,8 @@
-
+my_age = 4  # integer
+pi_value = 3.14  # float
+user_name = "JohnDoe"  # string
+list_95 = [10,5.3,"hi"] #list
+is_active = True  # boolean
+dictionaries = {"key": "fire","oto":"volvo"} # dict
+tupp= (5,52.4,"ja") # tuples
+sets = {"a","b","x"} # set
