59904236c8c0b50786687026d93e3106b4d64235
Akin-Inceler <akin2001inceler@gmail.com>
2025-03-13 11:40:14 +0000

    Add new file

--- student_details_structure.py
+++ student_details_structure.py
@@ -1,5 +1,9 @@
 # example
 student1 = {
- 'first_name':'Tommy',
-  'last_name':'Doe'
+ 'first_name':'Akin',
+  'last_name':'Inceler',
+  'index_number' : '35525',
+  'nationality': 'Turkiye',
+  'starting_date':'01.09.2024',
+  'courses':'mathematic,physics,software'
 }
